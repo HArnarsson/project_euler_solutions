0f089ea787497c438801c1e664e1cae1e13fd922
HArnarsson <hilmirvilberg@gmail.com>
2022-07-10 20:05:26 +0000

    Added problem 67

---
+++ solutions/001-100/061-070/peuler067.py
@@ -0,0 +1,16 @@
+with open('lib\\001-100\\peuler_resource067.txt','r') as triangle:
+    rows = []
+    for row in triangle:
+        row = row.split()
+        rows.append(row)
+rows = [rows[i] + ['0']*(len(rows[-1])-i-1) for i in range(len(rows))]
+for i in range(len(rows)):
+    for j in range(len(rows[i])):
+        rows[i][j] = int(rows[i][j])
+
+def find_max_path(triangle):
+    for i in range(len(triangle)-2,-1,-1):
+        for j in range(i+1):
+            triangle[i][j]+=max(triangle[i+1][j], triangle[i+1][j+1])
+    return triangle[0][0]
+print(find_max_path(rows))
