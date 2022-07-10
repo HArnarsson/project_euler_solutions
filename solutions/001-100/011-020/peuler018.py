fad0c3381ab2c5e3eab7ce16bc50a4f26602d1b7
HArnarsson <hilmirvilberg@gmail.com>
2022-07-10 19:56:52 +0000

    Added problem 18

--- solutions/001-100/011-020/peuler018.py
+++ solutions/001-100/011-020/peuler018.py
@@ -1,6 +1,17 @@
 with open('lib\\001-100\\peuler_resource018.txt','r') as triangle:
     rows = []
     for row in triangle:
+        row = row.split()
         rows.append(row)
-rows = [rows[i] + '0'*(len(rows[-1])-i) for i in range(len(rows))]
-print(rows)
+rows = [rows[i] + ['0']*(len(rows[-1])-i-1) for i in range(len(rows))]
+for i in range(len(rows)):
+    for j in range(len(rows[i])):
+        rows[i][j] = int(rows[i][j])
+
+def find_max_path(triangle):
+    for i in range(len(triangle)-2,-1,-1):
+        print(i)
+        for j in range(len(triangle[i])-(14-i)):
+            triangle[i][j]+=max(triangle[i+1][j], triangle[i+1][j+1])
+    return triangle[0][0]
+print(find_max_path(rows))
