57006b7779b1af93d77d1e219604107ef12cf140
HArnarsson <hilmirvilberg@gmail.com>
2022-07-10 19:26:36 +0000

    Testing

--- solutions/001-100/011-020/peuler018.py
+++ solutions/001-100/011-020/peuler018.py
@@ -2,5 +2,5 @@ with open('lib\\001-100\\peuler_resource018.txt','r') as triangle:
     rows = []
     for row in triangle:
         rows.append(row)
-rows = [rows[i] + '0'*len(rows[-1]-i) for i in range(len(rows))]
+rows = [rows[i] + '0'*(len(rows[-1])-i) for i in range(len(rows))]
 print(rows)
