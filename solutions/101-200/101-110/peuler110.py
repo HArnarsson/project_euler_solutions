c8ac30f3e71b8d1ba15067764bdcce92655445e6
HArnarsson <105976085+HArnarsson@users.noreply.github.com>
2022-07-22 13:03:55 +0000

    Update peuler110.py

--- solutions/101-200/101-110/peuler110.py
+++ solutions/101-200/101-110/peuler110.py
@@ -1,32 +1,43 @@
-# Project Euler - 110
-# Date: 21/07/2022
-
-# See resource for this problem for explanation
-from math import prod
+from math import inf, prod
 
 TARGET = 4*10**6
 from math import ceil
 min_dist = TARGET
-limit = 25
-nums = [3,5,7]
+limit = 15
+best_number = float(inf)
+def SieveOfEratosthenes(num):
+    prime = [True for i in range(num+1)]
+    p = 2
+    while (p * p <= num):
+        if (prime[p] == True):
+            for i in range(p * p, num+1, p):
+                prime[i] = False
+        p += 1
+    for p in range(2, num+1):
+        if prime[p]:
+            yield p
+
+def calc_num(best_exponents,primes):
+    best_factors = []
+    c=0
+    for i in range(len(best_exponents)-1,-1,-1):
+        for _ in range(best_exponents[i]):
+            for _ in range(i+1):
+                best_factors.append(primes[c])
+            c+=1
+    return prod(best_factors)
+
+primes = list(SieveOfEratosthenes(1000))
 for a in range(1,limit):
     for b in range(0,limit):
-        for c in range(0,limit):
-            for d in range(0,limit):
-                calc = ceil(1/2*(3**a)*(5**b)*(7**c)*(11**d))
-                if 0 < calc-TARGET < min_dist:
-                    best_exponents = [a,b,c]
-                    best_num = calc
-                    min_dist = calc-TARGET
+        for c in range(0,limit//2):
+            for d in range(0,limit//2):
+                calc = ceil(1/2*(3**a)*(5**b)*(7**c)*(9**d))
+                if calc>TARGET:
+                    if calc_num([a,b,c,d],primes) < best_number:
+                        best_exponents = [a,b,c,d]
+                        best_num = calc
+                        min_dist = calc-TARGET
+                        best_number = calc_num(best_exponents,primes)
 
-primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61]
-
-best_factors = []
-c=0
-for i in range(len(best_exponents)-1,-1,-1):
-    for _ in range(best_exponents[i]):
-        for _ in range(i+1):
-            best_factors.append(primes[c])
-        c+=1
-
-print(prod(best_factors))
+print(best_number)
